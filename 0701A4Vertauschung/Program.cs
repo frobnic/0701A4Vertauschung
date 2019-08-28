088dcf107cbf173403d42e743066a231297c4105
frank paulsen <frobnic+ita@gmail.com>
2019-08-28 15:17:05 +0200

    code inserted

--- 0701A4Vertauschung/Program.cs
+++ 0701A4Vertauschung/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _0701A4Vertauschung
+﻿namespace _0701A4Vertauschung
 {
     class Program
     {
+        static void schwapp(out int a, out int b)
+        {
+            int t;
+
+            if (a > b)
+            {
+                t = a;
+                a = b;
+                b = t;
+            }
+        }
+
         static void Main(string[] args)
         {
+            int x = 7, y = 2;
+
+            schwapp(out x, out y);
+
+            
         }
     }
 }
